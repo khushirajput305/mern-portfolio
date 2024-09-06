32e08df7598a1c1f2cfa7f6bcdd76b7ace7142f5
khushirajput305 <khushinurawat305@gmail.com>
2024-09-06 12:17:09 +0530

    work on whatsapp api

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,41 +1,59 @@
 import React from "react";
 import { useTheme } from "../../context/ThemeContext";
 import "./home.css";
-import { BsFillMoonStarsFill,BsFillSunFill } from "react-icons/bs";
+import { BsFillMoonStarsFill, BsFillSunFill } from "react-icons/bs";
 import Typewriter from "typewriter-effect";
 import Fade from "react-reveal/Fade";
 
-import Resume from '../../assets/docs/Resume.pdf'
+import Resume from "../../assets/docs/Resume.pdf";
 const Home = () => {
-  const [theme,setTheme] = useTheme()
+  const [theme, setTheme] = useTheme();
   //handle theme
-  const handleTheme=()=>{
-    setTheme((prevState)=>(prevState === 'light'? "dark":"light"));
-  }
+  const handleTheme = () => {
+    setTheme((prevState) => (prevState === "light" ? "dark" : "light"));
+  };
   return (
     <>
       <div className="container-fluid home-container" id="home">
         <div className="theme-btn" onClick={handleTheme}>
-          {theme==='light'?(<BsFillMoonStarsFill size={30}/>):(<BsFillSunFill size={30}/>)}
+          {theme === "light" ? (
+            <BsFillMoonStarsFill size={30} />
+          ) : (
+            <BsFillSunFill size={30} />
+          )}
         </div>
         <div className="container home-content">
           <Fade right>
-          <h2>Hi👋 I'm a </h2>
-          <h1>
-            <Typewriter
-              options={{
-                strings: ["FullStack Developer !", "MERN Stack Developer !"],
-                autoStart: true,
-                loop: true,
-              }}
-            />
-          </h1>
+            <h2>Hi👋 I'm a </h2>
+            <h1>
+              <Typewriter
+                options={{
+                  strings: ["FullStack Developer !", "MERN Stack Developer !"],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+            </h1>
           </Fade>
           <Fade bottom>
-          <div className="home-buttons">
-            <button className="btn btn-hire">Hire Me</button>
-            <a className="btn btn-cv" href={Resume} download={"your_name.pdf"}>My Resume</a>
-          </div>
+            <div className="home-buttons">
+              <a
+                className="btn btn-hire"
+                href="https://api.whatsapp.com/send?phone=8769497587"
+                rel="noreferrer"
+                target="_blank"
+              >
+                Hire Me
+              </a>
+
+              <a
+                className="btn btn-cv"
+                href={Resume}
+                download={"your_name.pdf"}
+              >
+                My Resume
+              </a>
+            </div>
           </Fade>
         </div>
       </div>
